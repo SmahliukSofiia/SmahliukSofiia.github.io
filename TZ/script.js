36644371c3233e09a70e4b21457c18396a954382
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-04-15 16:06:29 +0300

    Add files via upload

--- TZ/script.js
+++ TZ/script.js
@@ -5,23 +5,11 @@ function addPage(className) {
     document.body.insertBefore(block, document.body.firstChild);
 }
 
-addPage('screen screen--first');
-
-function addLogo(className) {
-    const container = document.getElementById(className);
-    const img = document.createElement('img');
-    img.src = 'img/Logotype.svg';
-    img.className = 'logo-image';
-    container.appendChild(img);
-}
-
-addLogo('screen screen--first');
-
 function removePage(className) {
     document.getElementById(className).remove();
 }
 
 setTimeout(() => {
-    removePage('screen screen--first');
-}, 5500);
+    removePage('screen1');
+}, 5000);
 
