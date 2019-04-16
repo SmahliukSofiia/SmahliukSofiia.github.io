6d57675e28379603c44c0100fce6b3d4e897aac7
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-04-16 11:23:34 +0300

    Add files via upload

--- TZ/script.js
+++ TZ/script.js
@@ -6,7 +6,6 @@ setTimeout(() => {
     removePage('screen1');
 }, 5000);
 
-
 function addPage(className) {
     let block = document.createElement('div');
     block.className = className;
@@ -18,9 +17,11 @@ addPage('screen screen--second');
 
 addPage('screen screen--third');
 
+function removePage(className) {
+    document.getElementById(className).remove();
+}
 
 setTimeout(() => {
-    removePage('screen screen--second');
     removePage('screen screen--third');
 }, 2000);
 
