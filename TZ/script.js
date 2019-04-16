c0c6814df21e5e12bd5569215af7661b6a897421
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-04-16 11:19:20 +0300

    Add files via upload
    Add Tzz

--- TZ/script.js
+++ TZ/script.js
@@ -1,10 +1,3 @@
-function addPage(className) {
-    let block = document.createElement('div');
-    block.className = className;
-    block.setAttribute('id', className);
-    document.body.insertBefore(block, document.body.firstChild);
-}
-
 function removePage(className) {
     document.getElementById(className).remove();
 }
@@ -12,4 +5,38 @@ function removePage(className) {
 setTimeout(() => {
     removePage('screen1');
 }, 5000);
+
+
+function addPage(className) {
+    let block = document.createElement('div');
+    block.className = className;
+    block.setAttribute('id', className);
+    document.body.insertBefore(block, document.body.firstChild);
+}
+
+addPage('screen screen--second');
+
+addPage('screen screen--third');
+
+
+setTimeout(() => {
+    removePage('screen screen--second');
+    removePage('screen screen--third');
+}, 2000);
+
+
+
+const menuBtn = document.getElementById('menuButton');
+
+menuBtn.addEventListener('click', () => {
+    const menu = document.getElementById('menu');
+    menu.style.display = 'block';
+})
+
+const crossBtn = document.getElementById('cross');
+
+crossBtn.addEventListener('click', () => {
+    const menu = document.getElementById('menu');
+    menu.style.display = 'none';
+})
 
