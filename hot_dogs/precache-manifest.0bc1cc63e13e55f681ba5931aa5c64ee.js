a861bf37cce2c5751c3481c1238f94dfd66ee204
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-06-22 15:11:43 +0300

    Add files via upload
    Add test task

---
+++ hot_dogs/precache-manifest.0bc1cc63e13e55f681ba5931aa5c64ee.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7c7dea2b6b90022fbb57443d270906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94547cba4a048823963b",
+    "url": "/static/css/main.6f284578.chunk.css"
+  },
+  {
+    "revision": "3935113cf568def15b28",
+    "url": "/static/js/2.22c693a9.chunk.js"
+  },
+  {
+    "revision": "94547cba4a048823963b",
+    "url": "/static/js/main.6bde94e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16f335d03a38e4abb0cdfa85924d0b69",
+    "url": "/static/media/OpenSans-Semibold.16f335d0.svg"
+  },
+  {
+    "revision": "22d4a36b7d366b2b213801c716dfb0b7",
+    "url": "/static/media/OpenSans-Semibold.22d4a36b.ttf"
+  },
+  {
+    "revision": "4c7b8237f45b448efb609f5d8e3997a9",
+    "url": "/static/media/OpenSans-Semibold.4c7b8237.woff"
+  },
+  {
+    "revision": "8165c1ec52bb9262b2105edc8032a5db",
+    "url": "/static/media/OpenSans-Semibold.8165c1ec.eot"
+  },
+  {
+    "revision": "b6a93c57aff7c0a760b81509194452b0",
+    "url": "/static/media/OpenSans-Semibold.b6a93c57.woff2"
+  },
+  {
+    "revision": "0071b00b4d686a9bf227e289c40d6f55",
+    "url": "/static/media/ProximaNova-Regular.0071b00b.woff2"
+  },
+  {
+    "revision": "4b64777c82b6dfb7a28c2ac7b5b17e46",
+    "url": "/static/media/ProximaNova-Regular.4b64777c.ttf"
+  },
+  {
+    "revision": "651c239adf29e56fd46137c9fef0b191",
+    "url": "/static/media/ProximaNova-Regular.651c239a.woff"
+  },
+  {
+    "revision": "76bb6ba97c2c13e79c04e3c9ed4cab27",
+    "url": "/static/media/ProximaNova-Regular.76bb6ba9.svg"
+  },
+  {
+    "revision": "db204ca7050f78c9054948b8e5a0b445",
+    "url": "/static/media/ProximaNova-Regular.db204ca7.eot"
+  },
+  {
+    "revision": "e0f6c15f302c66cd39ab54ad73c7da0e",
+    "url": "/static/media/photo_1.e0f6c15f.jpg"
+  },
+  {
+    "revision": "afebd9ed9641e3e9e450acc2f97f4476",
+    "url": "/static/media/photo_10.afebd9ed.jpg"
+  },
+  {
+    "revision": "a23c1ed2db1dd2abc922e1ee430ee2d5",
+    "url": "/static/media/photo_11.a23c1ed2.jpg"
+  },
+  {
+    "revision": "6521a49921b6bb7cef46e52f174fa207",
+    "url": "/static/media/photo_12.6521a499.jpg"
+  },
+  {
+    "revision": "4b3d4390946de34f89a13936fd397bf2",
+    "url": "/static/media/photo_2.4b3d4390.jpg"
+  },
+  {
+    "revision": "2d4718bace126984ee6fd115c3f57d6f",
+    "url": "/static/media/photo_3.2d4718ba.jpg"
+  },
+  {
+    "revision": "242ec9f4ac467c9ad59f3358bdcacfa6",
+    "url": "/static/media/photo_4.242ec9f4.jpg"
+  },
+  {
+    "revision": "ffd1599a0f6d36002312690ce4df6325",
+    "url": "/static/media/photo_5.ffd1599a.jpg"
+  },
+  {
+    "revision": "aef2946b767858afe68c662e20c0c72f",
+    "url": "/static/media/photo_6.aef2946b.jpg"
+  },
+  {
+    "revision": "22b27af8a31ec441b1956e204c928e1b",
+    "url": "/static/media/photo_7.22b27af8.jpg"
+  },
+  {
+    "revision": "2b786863bd2c26100b32420fb66b52e9",
+    "url": "/static/media/photo_8.2b786863.jpg"
+  },
+  {
+    "revision": "b86bb269e6626a89338b47514fec5a5f",
+    "url": "/static/media/photo_9.b86bb269.jpg"
+  }
+]);
