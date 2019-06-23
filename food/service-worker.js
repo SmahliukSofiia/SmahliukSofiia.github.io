141027e578548b7d17d986d5324a7cb78913bef9
Sofiia <46069177+SmahliukSofiia@users.noreply.github.com>
2019-06-23 03:38:12 +0300

    Add files via upload
    Add test task

--- food/service-worker.js
+++ food/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bc1cc63e13e55f681ba5931aa5c64ee.js"
+  "./precache-manifest.59287edc0e07e6c60fda6f2a5cad0443.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
